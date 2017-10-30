af0c06675e0bc8f338f2bb12fc337824cc8c55e9
Fangzhu-Tianji <zch793262201@163.com>
2017-10-30 15:13:07 +0800

    add

--- 金千禧公司简介--/pages/index/index.js
+++ 金千禧公司简介--/pages/index/index.js
@@ -1,65 +1,48 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    companyImg: [
+      "http://webapp.jinglih.com/images/wechat/jqx-1.jpg",
+      "http://webapp.jinglih.com/images/wechat/jqx-2.jpg",
+      "http://webapp.jinglih.com/images/wechat/jqx-3.jpg",
+      "http://webapp.jinglih.com/images/wechat/jqx-4.jpg",
+      "http://webapp.jinglih.com/images/wechat/jqx-5.jpg",
+      "http://webapp.jinglih.com/images/wechat/jqx-6.jpg"
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  onShareAppMessage: function (res) {
+    return {
+      title: '金融投资  多元产业',
+      path: '/page/index',
+      imageUrl: 'http://webapp.jinglih.com/images/wechat/jqx-6.jpg',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  openMap: function(){
+    wx.openLocation({
+      latitude: 22.54247,
+      longitude: 114.05601,
+      scale: 28,
+      name: '金千禧金融控股',
+      address: '深圳市福田区益田路6001号太平金融大厦28楼'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  openTel: function(){
+    wx.makePhoneCall({
+      phoneNumber: '0755-23947300'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  openImg: function(e){
+    wx.previewImage({
+      current: e.target.dataset.src,
+      urls: this.data.companyImg
+    })
   }
 })
